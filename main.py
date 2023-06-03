864a1e991dac743be05c7e0d9cde8775d8e7c0b3
nataliahanderek <72739947+nataliahanderek@users.noreply.github.com>
2023-06-03 12:08:45 +0200

    Statictics

--- main.py
+++ main.py
@@ -17,18 +17,21 @@ if __name__ == '__main__':
     # mongo_client = Mongo()
     # mongo_client.create_mongo()
 
-    # redis_client = Redis()
+    redis_client = Redis()
     # redis_client.create_redis()
 
-    sql_client = MySql()
+    # sql_client = MySql()
     # sql_client.create_single_tables()
     # sql_client.create_connection_table()
-
+    print(redis_client.count_words_in_titles())
     ### MONGO ###
 
     # time_select_mongo = count_time(lambda: mongo_client.select('London, Julia'))
     # print("Time for MONGODB: select:", time_select_mongo)
     #
+
+    # print(mongo_client.select('London, Julia'))
+    # print(mongo_client.select_all())
     # time_insert_mongo = count_time(lambda: mongo_client.insert())
     # print("Time for MONGODB: insert:", time_insert_mongo)
     #
@@ -45,8 +48,9 @@ if __name__ == '__main__':
     # time_select_redis = count_time(lambda: redis_client.select('London, Julia'))
     # print("Time for REDIS: select:", time_select_redis)
     #
-    # time_select_all_redis = count_time(lambda: redis_client.select_all())
-    # print(redis_client.select_all())
+
+    # time_select_all_redis = count_time(lambda: redis_client.select("London, Julia"))
+    # print(redis_client.select("London, Julia"))
     # print("Time for REDIS: select all:", time_select_all_redis)
     #
     # time_insert_redis = count_time(lambda: redis_client.insert())
@@ -60,8 +64,8 @@ if __name__ == '__main__':
 
     ### MYSQL ###
 
-    time_select_sql = count_time(lambda: sql_client.select('London, Julia'))
-    print("Time for SQL: select:", time_select_sql)
+    # time_select_sql = count_time(lambda: sql_client.select('London, Julia'))
+    # print("Time for SQL: select:", time_select_sql)
 
     # time_insert_sql = count_time(lambda: sql_client.insert())
     # print("Time for SQL: insert:", time_insert_sql)
--- script/redis_script.py
+++ script/redis_script.py
@@ -41,46 +41,6 @@ class Redis:
         # Wyłącz połączenie z bazą danych Redis
         self.r.close()
 
-    def select(self, author_name):
-        keys = self.r.keys()
-        titles = []
-
-        for key in keys:
-            data = self.r.hgetall(key)
-            if data[b'Author'] == author_name.encode():
-                titles.append(data[b'Title'].decode())
-
-        return titles
-
-    def select_all(self):
-        keys = self.r.keys()
-        titles = []
-
-        for key in keys:
-            data = self.r.hgetall(key)
-            titles.append(data[b'Title'].decode())
-
-        return titles
-
-    def delete(self, publication_year):
-        keys = self.r.keys()
-        for key in keys:
-            data = self.r.hgetall(key)
-            if data[b'PublicationYear'].decode() == publication_year:
-                self.r.delete(key)
-
-    def update(self, publication_year):
-        keys = self.r.keys()
-
-        for key in keys:
-            data = self.r.hgetall(key)
-            title = data.get(b'Title').decode('utf-8')
-            year = data.get(b'PublicationYear').decode()
-
-            if year == publication_year:
-                new_title = f"{title} ({year})"
-                self.r.hset(key, 'Title', new_title)
-
     def clear(self):
         self.r.flushdb()
 
@@ -98,3 +58,85 @@ class Redis:
             book_id = str(uuid.uuid4())
             key = f'book:{book_id}'
             self.r.hset(key, mapping=book_data)
+
+    def update(self, publication_year):
+        keys = self.r.keys()
+
+        for key in keys:
+            data = self.r.hgetall(key)
+            title = data.get(b'Title').decode('utf-8')
+            year = data.get(b'PublicationYear').decode()
+
+            if year == publication_year:
+                new_title = f"{title} ({year})"
+                self.r.hset(key, 'Title', new_title)
+
+    def select(self, author_name):
+        keys = self.r.keys()
+        results = []
+
+        for key in keys:
+            data = self.r.hgetall(key)
+            if data[b'Author'] == author_name.encode():
+                result = {k.decode(): v.decode() for k, v in data.items()}
+                results.append(result)
+
+        return results
+
+    def select_all(self):
+        keys = self.r.keys()
+        results = []
+
+        for key in keys:
+            data = self.r.hgetall(key)
+            result = {k.decode(): v.decode() for k, v in data.items()}
+            results.append(result)
+
+        return results
+
+    def delete(self, publication_year):
+        keys = self.r.keys()
+        for key in keys:
+            data = self.r.hgetall(key)
+            if data[b'PublicationYear'].decode() == publication_year:
+                self.r.delete(key)
+
+    def select_authors(self):
+        keys = self.r.keys()
+        authors = set()
+
+        for key in keys:
+            data = self.r.hgetall(key)
+            author = data[b'Author'].decode()
+            authors.add(author)
+
+        return list(authors)
+
+    def count_books_by_publisher_redis(self):
+        keys = self.r.keys()
+        counts = {}
+
+        for key in keys:
+            data = self.r.hgetall(key)
+            publisher = data[b'Publisher'].decode()
+
+            if publisher in counts:
+                counts[publisher] += 1
+            else:
+                counts[publisher] = 1
+
+        return counts
+
+    def count_words_in_titles(self):
+        keys = self.r.keys()
+        count = 0
+        search_word = 'for'
+
+        for key in keys:
+            data = self.r.hgetall(key)
+            if b'Title' in data and search_word in data[b'Title'].decode():
+                count += data[b'Title'].decode().count(search_word)
+
+        return count
+
+
